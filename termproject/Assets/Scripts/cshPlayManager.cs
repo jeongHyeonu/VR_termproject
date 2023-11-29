7e22904ce103580c2dca867f1c5e3742d1d496db
BellLine99 <kimbell_line99@naver.com>
2023-11-29 20:36:30 +0900

    feat: 타이머 및 점수 UI 추가, Sound 추가

--- termproject/Assets/Scripts/cshPlayManager.cs
+++ termproject/Assets/Scripts/cshPlayManager.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class cshPlayManager : MonoBehaviour
 {
-    public static int score = 0;
+    public TMP_Text timeText;
+    private float time;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        time = 60f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (time > 0) {
+            time -= Time.deltaTime;
+            timeText.text = Mathf.Ceil(time).ToString();
+        }
         
     }
 }
--- termproject/Assets/Scripts/cshThrowTrash.cs
+++ termproject/Assets/Scripts/cshThrowTrash.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class cshThrowTrash : MonoBehaviour
 {
     public string trashTag;
     private static int score = 0;
+    public TMP_Text scoreText;
+    public TMP_Text answerText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,13 +28,29 @@ public class cshThrowTrash : MonoBehaviour
             score += 10;
             Debug.Log(score);
             Destroy(coll.gameObject);
+            answerText.text = "O";
+            answerText.color = new Color(.2f, 1f, 0f, .7f);
+            SoundManager.instance.RecycleAnswerSound();
+            scoreText.text = "Score: " + score.ToString();
+            
+            StartCoroutine(TextOff());
         }
         else if (coll.gameObject.tag != trashTag)
         {
             score += 0;
             Debug.Log("Wrong! " + coll.gameObject.name + " Is not " + trashTag + " !!!");
             Destroy(coll.gameObject);
+            answerText.text = "X";
+            answerText.color = new Color(1f, 0f, 0f, .7f);
+            SoundManager.instance.RecycleWrongSound();
+            StartCoroutine(TextOff());
         }
+    }
+
+    IEnumerator TextOff()
+    {
+        yield return new WaitForSeconds(1.5f);
+        answerText.text = "";
     }
 
 }
